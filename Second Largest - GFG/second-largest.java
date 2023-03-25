b8a05d1edd42340d514c0f9ad974e2eeefee9c99
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2023-03-25 22:54:16 +0530

    Added solution - LeetHub

--- Second Largest - GFG/second-largest.java
+++ Second Largest - GFG/second-largest.java
@@ -36,23 +36,33 @@ class Solution {
     int print2largest(int arr[], int n) {
         // code here
         
-        int largestNum = arr[0];
-        int secondLargestNum = -1;
+        int largestNum = Integer.MIN_VALUE;           //arr[0];
+        int secondLargestNum = Integer.MIN_VALUE;     //-1;
         
-        for (int i = 1; i < n; i++) {
-            if (arr[i] > largestNum) {
-                // if (largestNum > secondLargestNum) {
-                    secondLargestNum = largestNum;
-                // }
-                largestNum = arr[i];
-            } else if (arr[i] < largestNum) {
-                int diff1 = largestNum - arr[i];
-                int diff2 = largestNum - secondLargestNum;
-                if (diff1 < diff2) {
-                    secondLargestNum = arr[i];
-                }
+        for (int i = 0; i < n; i++) {
+            // if (arr[i] > largestNum) {
+            //     // if (largestNum > secondLargestNum) {
+            //     secondLargestNum = largestNum;
+            //     // }
+            //     largestNum = arr[i];
+            // } else if (arr[i] < largestNum) {
+            //     int diff1 = largestNum - arr[i];
+            //     int diff2 = largestNum - secondLargestNum;
+            //     if (diff1 < diff2) {
+            //         secondLargestNum = arr[i];
+            //     }
+            // }
+            int val = arr[i];
+            
+            if (val > largestNum) {
+                secondLargestNum = largestNum;
+                largestNum = val;
+            } else if (val != largestNum && val > secondLargestNum) {
+                secondLargestNum = val;
             }
         }
+        
+        if (secondLargestNum == Integer.MIN_VALUE) return -1;
         
         return secondLargestNum;
     }
